fa9cb42fd808ab1caca5fad9bec1b31697635c4a
apple <areej.fatima2k@gmail.com>
2023-02-03 14:44:13 +0500

    frontend initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div classname="container">
+        <h1>Hello World!</h1>
+        <p>I just created my first React App</p>
+      </div>
+    );
+  }
+}
+export default App;
