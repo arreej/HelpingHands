6414f30266bf9c58c29a717fc7eb3de6f5a53970
apple <areej.fatima2k@gmail.com>
2023-02-06 15:34:43 +0500

    frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
-class App extends Component {
-  render() {
-    return (
-      <div classname="container">
-        <h1>Hello World!</h1>
-        <p>I just created my first React App</p>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route component={Navbar} exact path="/" />
       </div>
-    );
-  }
+    </Router>
+  )
 }
+
 export default App;
